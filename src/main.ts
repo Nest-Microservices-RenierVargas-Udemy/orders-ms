ecfa0cff06fd1bbf523d6cfbf868ad43706f235a
Renier Vargas <renier1989@gmail.com>
2024-09-04 06:11:52 -0500

    configuracion del orders-ms

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,19 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { envs } from './config';
 import { Logger } from '@nestjs/common';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const logger = new Logger('Orders-Microservice');
-  const app = await NestFactory.create(AppModule);
-  await app.listen(envs.port);
-  logger.log(`Ordets running on PORT: ${envs.port}`);
+
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule,{
+    transport: Transport.TCP,
+    options:{
+      port: envs.port
+    }
+  });
+
+  await app.listen();
+  logger.log(`Orders-MS running on PORT: ${envs.port}`);
 }
 bootstrap();
